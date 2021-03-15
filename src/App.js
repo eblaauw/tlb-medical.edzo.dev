087b97c54da7a2d58bfe47a1267b5876ceb3af28
Edzo <edzo@edzo.dev>
2021-03-15 16:39:58 +0400

    Small design fixes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
        <div className="container mx-auto px-4 md:px-0 font-sans lg:w-3/4 mt-8 md:mt-16">
-          <h1 className="text-4xl md:text-5xl font-bold relative font-title">talabat insurance checkerr</h1>
+          <h1 className="text-4xl md:text-5xl relative font-title">talabat <span className="font-bold">insurance checker</span></h1>
 
           <div className="w-full">
             <div className="mt-8 md:mt-8 mb-8 ">
